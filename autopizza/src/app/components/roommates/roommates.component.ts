6c9e3b85266ed8569145c075c81ba57d02e6801d
jacksongayda <jacksongayda@gmail.com>
2020-08-03 13:46:34 -0700

    Set up ripples and profile bubbles for new order page

--- autopizza/src/app/components/roommates/roommates.component.ts
+++ autopizza/src/app/components/roommates/roommates.component.ts
@@ -1,19 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { RoommatesService, Roommate } from './../../services/roommates.service';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-roommates',
   templateUrl: './roommates.component.html',
-  styleUrls: ['./roommates.component.scss']
+  styleUrls: ['./roommates.component.scss'],
 })
 export class RoommatesComponent implements OnInit {
-
   roommateList: Roommate[];
+  roommateToOrderMap: Map<Roommate, boolean> = new Map();
+  roommateWidth: number;
+  roommatePadding: 2;
 
-  constructor(private roommateService: RoommatesService) { }
+  constructor(private roommateService: RoommatesService) {}
 
   ngOnInit() {
     this.roommateList = this.roommateService.asList;
+    for (const roommate of this.roommateList) {
+      this.roommateToOrderMap.set(roommate, false);
+      console.log(`Setting ${roommate.firstName} to false...`);
+    }
+    this.generateCssDimensions();
   }
 
+  generateCssDimensions() {
+    const padding = this.roommatePadding * this.roommateList.length * 2;
+    this.roommateWidth = (100 / this.roommateList.length) - padding;
+  }
+
+  isIncludedInOrder(roommate: Roommate): boolean {
+    return this.roommateToOrderMap.get(roommate);
+  }
+
+  toggleIncludeInOrder(roommate: Roommate): void {
+    const isIncluded = this.roommateToOrderMap.get(roommate);
+    this.roommateToOrderMap.set(roommate, !isIncluded);
+  }
 }
